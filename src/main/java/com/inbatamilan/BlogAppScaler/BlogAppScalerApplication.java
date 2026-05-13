18b21de036a4dea8300c799d3f7d9c6eeb008c02
Inbatamilan <inbatamilanhk10@gmail.com>
2026-05-13 18:26:03 +0530

    initial project setup
    Signed-off-by: Inbatamilan <inbatamilanhk10@gmail.com>

---
+++ src/main/java/com/inbatamilan/BlogAppScaler/BlogAppScalerApplication.java
@@ -0,0 +1,13 @@
+package com.inbatamilan.BlogAppScaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppScalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppScalerApplication.class, args);
+	}
+
+}
